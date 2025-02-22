d9ea0c0b5fc3c1ebfb68422a0d87d42fd2d00448
Jermoy <cjermoy4@gmail.com>
2025-02-22 17:11:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// app.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcjlRrl_Lfoma3b7ue9lqX9O81ctgWcAo",
+  authDomain: "soonmove-a1f40.firebaseapp.com",
+  projectId: "soonmove-a1f40",
+  storageBucket: "soonmove-a1f40.firebasestorage.app",
+  messagingSenderId: "781161740833",
+  appId: "1:781161740833:web:b8caef79c1a70c235b0160",
+  measurementId: "G-G8MQP6GHBE"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // User is logged in, redirect to dashboard
     window.location.href = '/dashboard.html';
   } else {
-    // User is logged out, redirect to login
     window.location.href = '/login.html';
   }
 });
